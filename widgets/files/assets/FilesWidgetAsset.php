bd7fe8bcb854067f1b909c4544d5098badfe795d
zrk4939@gmail.com <zrk4939@gmail.com>
2018-03-25 03:38:39 +0300

    fix asset path

--- widgets/files/assets/FilesWidgetAsset.php
+++ widgets/files/assets/FilesWidgetAsset.php
@@ -13,7 +13,7 @@ use yii\web\AssetBundle;
 
 class FilesWidgetAsset extends AssetBundle
 {
-    public $sourcePath = '@zrk4939/modules/files/widget/assets/dist';
+    public $sourcePath = '@zrk4939/modules/files/widgets/files/assets/dist';
 
     public $js = [
         'files-widget.js',
